7073bdf62096b2cbebc33a67512a337f4abe4507
gluck4668 <109885777+gluck4668@users.noreply.github.com>
2024-06-26 12:43:27 +0800

    Add files via upload

---
+++ 7-forest_example.R
@@ -0,0 +1,14 @@
+
+library(LXbatmr)
+
+
+rm(list=ls())
+
+or_dir ="6-5"
+
+mr_method=c("Inverse variance weighted")     #选择展示的方法
+
+#devtools::load_all()
+
+#---------------------------------
+fores_plot()
