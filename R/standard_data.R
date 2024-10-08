2c7c2b2001e41048ea2d2616f20bf8419f9c3277
gluck4668 <109885777+gluck4668@users.noreply.github.com>
2024-10-08 23:13:23 +0800

    Add files via upload

--- R/standard_data.R
+++ R/standard_data.R
@@ -28,11 +28,25 @@ foreach(x=c(1:length(gwas_file)), .errorhandling = "pass") %do% {
 
   colnames(df)[which(colnames(df)==pos)] <- "base_pair_location"
   colnames(df)[which(colnames(df)==SNP)] <- "variant_id"
+
   colnames(df)[which(colnames(df)==beta)] <- "beta"
+
+  # beta = log(OR), 如果只提供OR，需要转换成log(OR)，才是beta
+  if(!grepl("Log", beta,ignore.case = T)) {
+      if(grepl("odd",beta,ignore.case = T) | grepl("OR",beta,ignore.case = T)){
+        df$beta <- log(as.numeric(df$beta))
+      }
+    }
+
   colnames(df)[which(colnames(df)==pval)] <- "p_value"
   colnames(df)[which(colnames(df)==se)] <- "standard_error"
+
   colnames(df)[which(colnames(df)==effect_allele)] <- "effect_allele"
+  df$effect_allele <- toupper(df$effect_allele)
+
   colnames(df)[which(colnames(df)==other_allele)] <- "other_allele"
+  df$other_allele <- toupper(df$other_allele)
+
   colnames(df)[which(colnames(df)==eaf)] <- "effect_allele_frequency"
 
   fwrite(df, paste0(str_extract(gwas_file[x],".*(?=\\.)"),"_standard.gz"))
