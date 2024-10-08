5fbfb8a5260429c97fab71928a9ceb293a84a41d
gluck4668 <109885777+gluck4668@users.noreply.github.com>
2024-10-09 01:22:18 +0800

    Add files via upload

--- 5_clear_data_example.R
+++ 5_clear_data_example.R
@@ -6,7 +6,7 @@ gc()
 
 # devtools::load_all()
 
-data_dir <- "D:/Desktop/LXbatmr 2024-9-21(v3.17)/data/exp_gwas"
+data_dir <- "D:/Desktop/LXbatmr 2024-10-08(v3.36)/20241008/exp1008"
 
 # library(data.table)
 # head(fread(dir(data_dir,full.names = T)[1]))
@@ -22,9 +22,6 @@ eaf_exp = "effect_allele_frequency"
 pval_exp = "p_value"
 #---------------------------------
 
-samplesize_info= "D:/Desktop/LXbatmr 2024-9-21(v3.17)/exp_gwas_gwas_id_info.xlsx" # "The 233 circulating metabolic biomarkers buildGRCh.xlsx_gwas_info.xlsx" # 先运行：1_gwas_info_example （获取样本数量）
-
-#--------------------------------
 clum_p=1e-5
 clump_kb=10000
 clump_r2=0.001
@@ -38,7 +35,6 @@ clear_exp_data(data_dir=data_dir,snp_exp=snp_exp,beta_exp=beta_exp,
                effect_allele_exp=effect_allele_exp,
                other_allele_exp=other_allele_exp,
                eaf_exp=eaf_exp,pval_exp=pval_exp,
-               samplesize_info=samplesize_info,
                clum_p=clum_p,clump_kb=clump_kb,clump_r2=clump_r2)
 
 
