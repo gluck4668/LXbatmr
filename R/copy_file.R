155d4e0830589d8395cdd213553d1b50a2574403
gluck4668 <109885777+gluck4668@users.noreply.github.com>
2024-10-24 08:01:38 +0800

    Add files via upload

--- R/copy_file.R
+++ R/copy_file.R
@@ -6,7 +6,7 @@ copy_file <- function(file_list,from_dir,to_dir){
 #---R packages--------
   com_packages()
 
-#------------
+#--------------------
 
 file.list <- read.xlsx(file_list) %>% .[,1] %>% unique()
 from.list <- dir(from_dir)
@@ -18,12 +18,20 @@ nonfile <-data.frame()
 
 foreach(x=file.list,.errorhandling = "pass") %do% {
 
+if(grepl("\\.",x)){
+    x <- str_extract(x,".*?(?=\\.)")
+  }
+
+if(grepl("_",x)){
+  x <- str_extract(x,".*?(?=_)")
+}
+
 print(paste0(x, " is being copied......"))
+
 if(!any(grepl(x,from.list,ignore.case = T))){
   message (paste0(x, " can not be found in the '",from_dir,"' folder"))
   nonfile <- rbind(nonfile,x)
   }
-
 
 copy.file <- grep(x,from.list,ignore.case = T,value = T)
 
